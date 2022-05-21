e7496db2f7a69522a7a101be7019b4e69701fef0
AdeRegt <alexandrosderegtvisvliet@gmail.com>
2022-05-21 21:20:56 +0200

    Adding more information

--- src/assembler/headers/core.h
+++ src/assembler/headers/core.h
@@ -6,6 +6,23 @@ typedef struct CommandoArgumentIn{
     struct CommandoArgumentIn *next;
 }CommandoArgumentIn;
 
+/**
+ * @brief Parses the sourcefile and returns a linkedlist to the source
+ * 
+ * @return struct CommandoArgumentIn* 
+ */
 struct CommandoArgumentIn *parseSourceFile();
+
+/**
+ * @brief Get the File Size
+ * 
+ * @return int the size of the inputfile
+ */
 int getFileSize();
+
+/**
+ * @brief Get the Source File Buffer
+ * 
+ * @return char* the inputfilebuffer
+ */
 char* getSourceFileBuffer();
